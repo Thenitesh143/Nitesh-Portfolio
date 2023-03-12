9a857ea0a0b11f5a8f736e6ec98785f5200d07a1
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-12 17:18:44 +0530

    Add hackerbook as project

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -5,6 +5,7 @@ import './Project.css'
 import projectimg from '../../Assets/project.jpg'
 import hotstar from '../../Assets/projectimg/hotstar.png'
 import beats from '../../Assets/projectimg/beats.png'
+import hackerbook from '../../Assets/projectimg/hackerbook.png'
 
 const Project = () => {
     return (
@@ -12,10 +13,10 @@ const Project = () => {
             <img src={projectimg} alt="" />
             <div className="display-project">
                 <div className='project-content'>
-                    <h3>Beat</h3>
-                    <p>A website for  award-winning Beats sound with wireless listening freedom and a sleek, streamlined design with comfortable padded earphones, delivering first-rate playback.  </p>
+                    <h3>Hackerbook </h3>
+                    <p>A website for  learning hacking and penetration testing. Build for a bussiness client. This project uses Material UI for deesign as well as a major role of vanilla  CSS can also be seeen. </p>
                 </div>
-                <div><a href="https://steady-gingersnap-64e01e.netlify.app/"><img src={beats} alt="" /></a>
+                <div><a href="https://hackerbook-classroom.netlify.app/"><img src={hackerbook} alt="" /></a>
                 </div>
             </div>
             <div className="display-project second-pro">
@@ -24,6 +25,14 @@ const Project = () => {
                     <p>Disney+ Hotstar is an online video streaming platform owned by Novi Digital Entertainment Private Limited, a wholly owned subsidiary of Star India Private Limited.  </p>
                 </div>
                 <div><a href="https://steady-gingersnap-64e01e.netlify.app/"><img src={hotstar} alt="" /></a>
+                </div>
+            </div>
+            <div className="display-project">
+                <div className='project-content'>
+                    <h3>Beat</h3>
+                    <p>A website for  award-winning Beats sound with wireless listening freedom and a sleek, streamlined design with comfortable padded earphones, delivering first-rate playback.  </p>
+                </div>
+                <div><a href="https://steady-gingersnap-64e01e.netlify.app/"><img src={beats} alt="" /></a>
                 </div>
             </div>
         </div>
